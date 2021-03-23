631b8b78054fdbafb72e72f208945dd51e1a0a3e
mayank <mynkpl1998@gmail.com>
2021-03-23 22:00:43 +0530

    Removed the slower implementation of search template

--- utils.py
+++ utils.py
@@ -246,39 +246,6 @@ class Frame:
                                                                                         self.checksum,
                                                                                         "PASS" if self.is_checksum_valid else "FAIL")
 
-def search_sequence_numpy(arr,seq):
-    """ Find sequence in an array using NumPy only.
-
-    Source: https://stackoverflow.com/questions/36522220/searching-a-sequence-in-a-numpy-array
-
-    Parameters
-    ----------    
-    arr    : input 1D array
-    seq    : input 1D array
-
-    Output
-    ------    
-    Output : 1D Array of indices in the input array that satisfy the 
-    matching of input sequence in the input array.
-    In case of no match, an empty list is returned.
-    """
-
-    # Store sizes of input array and sequence
-    Na, Nseq = arr.size, seq.size
-
-    # Range of sequence
-    r_seq = np.arange(Nseq)
-
-    # Create a 2D array of sliding indices across the entire length of input array.
-    # Match up with the input sequence & get the matching starting indices.
-    M = (arr[np.arange(Na-Nseq+1)[:,None] + r_seq] == seq).all(1)
-
-    # Get the range of those indices as final output
-    if M.any() >0:
-        return np.where(np.convolve(M,np.ones((Nseq),dtype=int))>0)[0]
-    else:
-        return []         # No match found
-
 
 class lowPassFilter:
 
